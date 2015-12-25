61b2cfc549332449fe555deefc3db96757abd4e0
DimaDen94 <DimaDen94@MAIL.RU>
2015-12-25 07:25:58 +0200

    star

--- StarWarsReminder/app/src/main/java/dmitrydenezho/com/starwarsreminder/MainActivity.java
+++ StarWarsReminder/app/src/main/java/dmitrydenezho/com/starwarsreminder/MainActivity.java
@@ -1,35 +1,32 @@
 package dmitrydenezho.com.starwarsreminder;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
 private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
-
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.title_toolbar);
+        toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
+            public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;
             }
         });
         toolbar.inflateMenu(R.menu.menu_main);
     }
-
 
 }
